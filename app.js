daea2da9dfd4d464817791f517014eaa54afacc3
al1211 <kanil013623@gmail.com>
2025-09-06 14:44:58 +0530

    add frontend url

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const app=express();
 app.use(express.json());
 app.use(cookieParser())
 app.use(cors({
-  origin:"http://localhost:5173",
+  origin:"https://course-bloger.netlify.app/",
   credentials:true
 }))
 app.use((req, res, next) => {
